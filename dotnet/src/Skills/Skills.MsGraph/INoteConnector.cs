6a0672429a1e35d6d04d2a332aa0ba9b5f37c44a
Martin Gudgin <mgudgin@microsoft.com>
2023-05-09 16:34:14 -0700

    Start on adding OneNote skill to msgraph

---
+++ dotnet/src/Skills/Skills.MsGraph/INoteConnector.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Skills.MsGraph;
+
+/// <summary>
+/// Interface for notes documents (e.g. OneNote).
+/// </summary>
+public interface INoteConnector
+{
+    /// <summary>
+    /// Get the content of a page.
+    /// </summary>
+    /// <param name="name">Name of the notebook.</param>
+    /// <param name="path">Path to the page.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    Task<Stream> GetPageContentStreamAsync(string name, string path, CancellationToken cancellationToken = default);
+}
